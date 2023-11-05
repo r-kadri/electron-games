1cd0b72654856bd4c19e89df6815a5fe5ee1fba1
Ryan Kadri <ryankadri.dev@gmail.com>
2023-11-05 23:36:51 +0100

    :tada: Init project

---
+++ preload.js
@@ -0,0 +1,10 @@
+window.addEventListener('DOMContentLoaded', () => {
+    const replaceText = (selector, text) => {
+      const element = document.getElementById(selector);
+      if (element) element.innerText = text;
+    };
+  
+    replaceText('framework', 'Electron');
+});
+
+console.log('preload.js loaded');
