cbce1a0d9f3f810094cd911fae90132d456b6e7b
Ryan Kadri <ryankadri.dev@gmail.com>
2023-11-11 01:06:18 +0100

    :wrench: ADD: React framework with typescript

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 const { app, BrowserWindow } = require('electron');
 const path = require('node:path');
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 const createWindow = () => {
     const win = new BrowserWindow({
@@ -9,8 +12,12 @@ const createWindow = () => {
         preload: path.join(__dirname, 'preload.js')
       }
     });
-  
-    win.loadFile('index.html');
+
+    win.webContents.openDevTools();
+
+    process.env.NODE_ENV === 'production'
+      ? win.loadFile(__dirname + '/app/build/index.html')
+      : win.loadURL('http://localhost:3000');
 };
 
 app.whenReady().then(() => {
--- preload.js
+++ preload.js
@@ -1,10 +1,10 @@
-window.addEventListener('DOMContentLoaded', () => {
-    const replaceText = (selector, text) => {
-      const element = document.getElementById(selector);
-      if (element) element.innerText = text;
-    };
+// window.addEventListener('DOMContentLoaded', () => {
+//     const replaceText = (selector, text) => {
+//       const element = document.getElementById(selector);
+//       if (element) element.innerText = text;
+//     };
   
-    replaceText('framework', 'Electron');
-});
+//     replaceText('framework', 'Electron');
+// });
 
 console.log('preload.js loaded');
